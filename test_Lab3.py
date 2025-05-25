0c51b60db8b817078c156a265f7afa7e1a9e99bc
zayanfm <izzzayan.24@ichat.sp.edu.sg>
2025-05-26 03:28:45 +0800

    add more

--- test_Lab3.py
+++ test_Lab3.py
@@ -21,6 +21,7 @@ def test_bubble_sort_descending():
 
     assert (result == test_arr)
 
+
 def test_bubble_sort_invalid():
     result = []
     input_arr = [64, 34, 25, 12, 22, 11, 90]
@@ -28,3 +29,36 @@ def test_bubble_sort_invalid():
     result = Lab3.bubble_sort(input_arr, 3)
 
     assert (result == [])
+
+def test_bubble_sort_more_than_10():
+    result = []
+    input_arr = [64, 34, 25, 12, 22, 11, 90, 88, 75, 43, 23]
+
+    result = Lab3.bubble_sort(input_arr, Lab3.SORT_ASCENDING)
+
+    assert (result == 1)
+
+def test_bubble_sort_less():
+    result = []
+    input_arr = [64, 34, 25, 12, 22]
+    test_arr = [12, 22, 25, 34, 64]
+
+    result = Lab3.bubble_sort(input_arr, Lab3.SORT_ASCENDING)
+
+    assert(result == test_arr)
+
+def test_bubble_check_int():
+    result = []
+    input_arr = [45, 10, 20, 15, "dog", 6, 7]
+
+    result = Lab3.bubble_sort(input_arr, Lab3.SORT_ASCENDING)
+
+    assert(result == 2)
+
+def test_bubble_sort_empty():
+    result = []
+    input_arr = []
+
+    result = Lab3.bubble_sort(input_arr, Lab3.SORT_ASCENDING)
+
+    assert(result == 0)
