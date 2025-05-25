eaff9198b45e1d9562c9419702745e303f5d7cd7
zayanfm <izzzayan.24@ichat.sp.edu.sg>
2025-05-26 04:02:10 +0800

    employee updates

--- employee_info.py
+++ employee_info.py
@@ -23,15 +23,20 @@ def calculate_average_salary():
     average = 0
 
     #add your implementation to calculate here
+    for item in employee_data:
+        total += item["salary"]
 
+    average = total / len(employee_data)
 
-    return average
+    return round(average, 2)
 
 def get_employees_by_dept(department):
     result = []
 
     # Add your implementation from here
-
+    for item in employee_data:  
+        if item["department"] == department:
+            result.append(item)
 
     return result
 
